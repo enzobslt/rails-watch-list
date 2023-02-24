a7f07e80eff2fb555a3734c17d2fc974993444d3
enzo boistault <enzo.boistault@gmail.com>
2023-02-24 13:05:50 +0100

    iyvuc

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,7 +1,8 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
-
+  has_one_attached :photo
+  
   validates :comment, length: { minimum: 6 }
   validates :list_id, uniqueness: { scope: :movie_id }
   validates :movie_id, presence: true
